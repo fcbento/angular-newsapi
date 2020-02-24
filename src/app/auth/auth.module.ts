eab92b00f14661ad05bc3f79d02833794314d061
Felipe Costa <felipe.costa@resourceit.com>
2020-02-24 19:59:45 -0300

    #TASK-003 - Sign In Page - Validations and refactoring

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -4,7 +4,6 @@ import { AuthRoutingModule } from './auth.routing.module';
 import { DirectiveModule } from '../directives/directive.module';
 import { AuthComponent } from './auth.component';
 import { SignInComponent } from './sign-in/sign-in.component';
-import { SignUpComponent } from './sign-up/sign-up.component';
 import { MaterialModule } from '../material/material.module';
 import { HeadTitleComponent } from './head-title/head-title.component';
 import { SocialMediaComponent } from './social-media/social-media.component';
@@ -14,7 +13,6 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
   declarations: [
     AuthComponent,
     SignInComponent,
-    SignUpComponent,
     HeadTitleComponent,
     SocialMediaComponent
   ],
--- src/app/auth/sign-in/sign-in.component.ts
+++ src/app/auth/sign-in/sign-in.component.ts
@@ -10,17 +10,49 @@ import EmailValidator from '../../utils/email.validator';
 export class SignInComponent implements OnInit {
   
   public form: FormGroup;
+  public signInType: boolean = true;
+  public passwordDontMatch: boolean = false;
+  public showSignType: boolean = true;
+  public signInfo: string = '';
+  public signAction: string = '';
+  public signActionBtn: string = '';
   
   constructor(public formBuilder: FormBuilder) {
     this.form = this.formBuilder.group({
       email: ['', [Validators.required, Validators.pattern(EmailValidator())]],
-      password: ['', [Validators.required, Validators.minLength(6)]]
+      password: ['', [Validators.required, Validators.minLength(6)]],
+      repassword: ['', [Validators.required, Validators.minLength(6)]]
     });
    }
 
   ngOnInit(): void {
+    this.checkPage();
   }
 
-  onSignIn() {}
+  public onSignIn() {
+    
+  }
+
+  public checkPage(){
+    this.showSignType ? this.signInfo = 'Dont have an account?' : this.signInfo = 'Already have an account?';
+    this.showSignType ? this.signAction = 'Sign Up now' : this.signAction = 'Sign In'
+    this.showSignType ? this.signActionBtn = 'Sign In' : this.signActionBtn = 'Sign Up'
+
+  }
+
+  public signType(){
+    this.showSignType = !this.showSignType;
+    this.form.reset();
+    this.checkPage();
+  }
+
+  public passwordsDontMatch (){
+    
+    if(this.form.value.password !== this.form.value.repassword){
+      this.passwordDontMatch = !this.passwordDontMatch;
+      return true
+    }
+  }
+
 
 }
