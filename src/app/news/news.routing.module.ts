4f73e05c362d52cb2067525add37f69396dbc9b6
Felipe Costa <felipe.costa@resourceit.com>
2020-03-02 13:47:10 -0300

    #TASK-009 - Do not allow unauthenticated users on the news page

--- src/app/news/news.routing.module.ts
+++ src/app/news/news.routing.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { NewsComponent } from './news.component';
 import { NewsCategoryComponent } from './news-category/news-category.component';
+import { AuthGuardService } from '../services/auth-guard.service';
 
 const routes: Routes = [
   {
-    path: 'news', component: NewsComponent,
+    path: 'news', component: NewsComponent, canActivate: [AuthGuardService],
     children: [
       { path: '', component: NewsCategoryComponent },
       { path: 'category', component: NewsCategoryComponent }
